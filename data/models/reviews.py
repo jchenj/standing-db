dd4e7e1e097071dc3c9072bd414ab950562adc08
Julianna Chen <c.julianna@gmail.com>
2020-05-17 14:12:42 +0800

    finalize reviews data model

--- data/models/reviews.py
+++ data/models/reviews.py
@@ -3,13 +3,15 @@ import datetime
 from sqlalchemy import orm
 from data.sqlalchemybase import SqlAlchemyBase
 
-#! TODO: fill in columns
+
 class Review(SqlAlchemyBase):
     class Review(SqlAlchemyBase):
         __tablename__: "reviews"
-        id = sa.Column()
-        created_date = sa.Column()
-        reviewer = sa.Column()
-        comments = sa.column()
+
+        id = sa.Column(sa.Integer, primary_key=True, autoincrement=True)
+        created_date = sa.Column(sa.DateTime, default=datetime.datetime.now, index=True)
+        reviewer = sa.Column(sa.String, index=True)
+        comments = sa.column(sa.String)
+
         location_id = sa.Column(sa.Integer, sa.ForeignKey('locations.id'), nullable=False)
         location = orm.relation('Location', back_populates='review')
