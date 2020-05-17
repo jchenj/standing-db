8298215b76e37094ef729a84666b93d3aae44762
Julianna Chen <c.julianna@gmail.com>
2020-05-17 13:57:58 +0800

    finalize locations data model

--- data/models/locations.py
+++ data/models/locations.py
@@ -3,19 +3,26 @@ import datetime
 from sqlalchemy import orm
 from data.sqlalchemybase import SqlAlchemyBase
 
-#! TODO: fill in columns
+
 class Location(SqlAlchemyBase):
     __tablename__: "locations"
-    id = sa.Column()
-    created_date = sa.Column()
-    org = sa.Column()
-    street = sa.Column()
-    city = sa.Column()
-    state = sa.Column()
-    country = sa.Column()
-    height = sa.Column()
-    adjustsable = sa.Column()
-    seating = sa.Column()
-    power = sa.Column()
-    foodbev = sa.Column()
-    comments = sa.column()
+
+    id = sa.Column(sa.Integer, primary_key=True, autoincrement=True)
+
+    created_date = sa.Column(sa.DateTime, default=datetime.datetime.now, index=True)
+    org = sa.Column(sa.String, index=True)
+    street = sa.Column(sa.String)
+    city = sa.Column(sa.String, index=True)
+    state = sa.Column(sa.String)
+    country = sa.Column(sa.String, index=True)
+
+    height_in = sa.Column(sa.Integer)
+    height_cm = sa.Column(sa.Integer)
+    standing_capacity = sa.Column(sa.Integer)
+    adjustable = sa.Column(sa.Boolean)
+    seating = sa.Column(sa.Boolean)
+    power = sa.Column(sa.Boolean)
+    foodbev = sa.Column(sa.Boolean)
+    comments = sa.column(sa.String)
+
+    reviews = orm.relation('Review', back_populates='location')
